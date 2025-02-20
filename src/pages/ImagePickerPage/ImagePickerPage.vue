bd7cb7f6455ab65bac9182d89c48068e9a8f6be0
Yura Ya <yura@revpush.cc>
2025-02-20 15:05:09 +0300

    feat: AppImage

--- src/pages/ImagePickerPage/ImagePickerPage.vue
+++ src/pages/ImagePickerPage/ImagePickerPage.vue
@@ -2,28 +2,79 @@
 import PageWrapper from "@/shared/ui/PageWrapper/PageWrapper.vue";
 import FilePond from "@/widgets/ImagePicker/FilePond/FilePond.vue";
 
+import { IMG_BLOB_DATA, BASE64_IMG } from "./data";
+import AppImage from "@/shared/ui/AppImage/AppImage.vue";
+
+import { IMG_BLOB_DATA, BASE64_IMG } from "./data";
+
 export default {
   components: {
     PageWrapper,
     FilePond,
+    AppImage,
   },
 
   data() {
     return {
-      titles: {
-        filePond: "FilePond:",
-      },
+      filePondTitle: "FilePond",
       info: ["PQina FilePond: https://github.com/pqina/vue-filepond"],
+
+      base64Img: BASE64_IMG,
+      blobImg: null,
+      urlImg: "https://html.com/wp-content/uploads/flamingo.jpg",
     };
+  },
+
+  mounted() {
+    this.fetchImage();
+  },
+
+  methods: {
+    fetchImage() {
+      fetch(IMG_BLOB_DATA)
+        .then((response) => response.blob())
+        .then((blob) => {
+          this.blobImg = URL.createObjectURL(blob);
+        })
+        .catch((error) => {
+          console.error("img receiving error:", error);
+        });
+    },
   },
 };
 </script>
 
 <template>
   <PageWrapper :title="'Image Picker'" :info="info">
-    <div class="section">
-      <h4 class="section-title">{{ titles.filePond }}</h4>
+    <!-- <div class="section">
+      <h4 class="section-title">{{ filePondTitle }}</h4>
       <FilePond />
+    </div> -->
+
+
+    <hr />
+    <div>
+      <div class="section">
+        <h4 class="section-title">base64</h4>
+        <div>
+          <AppImage :src="base64Img" :alt="'Base64 Image'" />
+        </div>
+      </div>
+
+      <div class="section">
+        <h4 class="section-title">urlImg</h4>
+        <div>
+          <AppImage :src="urlImg" :alt="'Base64 Image'" />
+        </div>
+      </div>
+
+      <div v-if="blobImg" class="section">
+        <hr />
+        <h4 class="section-title">blob</h4>
+        <div>
+          <img :src="blobImg" alt="Blob Image" />
+        </div>
+      </div>
     </div>
   </PageWrapper>
 </template>
---
+++ src/shared/ui/AppImage/AppImage.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+    },
+    alt: {
+      type: Text,
+    },
+  },
+
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <div class="img-wrapper">
+    <img class="img" :src="src" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
